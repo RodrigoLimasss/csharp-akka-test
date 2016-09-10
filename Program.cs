704f204d291f8e58043fb04d773bdd38420feeff
Rodrigo Lima <rodrigolimasss@gmail.com>
2016-09-10 17:06:20 -0300

    Creating Initial Project

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Akka.Actor;
+
+namespace csharp_akka_test
+{
+    class Program
+    {
+        private static ActorSystem _actorSystem;
+
+        static void Main(string[] args)
+        {
+            _actorSystem = ActorSystem.Create("myActorSystem");
+
+            Console.WriteLine("Hello Akka: {0}", _actorSystem.Name);
+
+            Console.ReadLine();
+        }
+    }
+}
