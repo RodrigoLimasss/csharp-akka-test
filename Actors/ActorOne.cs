eff2397467e1eb8ac445a8507c5270605a53fa6b
Rodrigo Lima <rodrigolimasss@gmail.com>
2016-09-11 22:26:58 -0300

    Override actor hook methods

--- Actors/ActorOne.cs
+++ Actors/ActorOne.cs
@@ -17,5 +17,31 @@ namespace csharp_akka_test.Actors
         {
            Console.WriteLine(string.Format("{0}, {1}", message.Name, message.Id));
         }
+
+        protected override void PreStart()
+        {
+            //base.PreStart();
+            Console.WriteLine("PreStart: before actor receive first message.");
+        }
+
+        protected override void PostStop()
+        {
+            //base.PostStop();
+            Console.WriteLine("PostStop: after the actor has been stopped.");
+        }
+
+        protected override void PreRestart(Exception reason, object message)
+        {
+            Console.WriteLine("PreRestart: before actor begins restarting.");
+            Console.WriteLine("PreRestart reason: " + reason);
+            base.PreRestart(reason, message);
+        }
+
+        protected override void PostRestart(Exception reason)
+        {
+            Console.WriteLine("PostRestart: after PreRestart() and before PreStart();");
+            Console.WriteLine("PostRestart reason: " + reason);
+            base.PostRestart(reason);
+        }
     }
 }
